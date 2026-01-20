00e76c89208963d5314f577283b38faf0a0673d1
AlexDev101 <93138539+AlexDev101@users.noreply.github.com>
2026-01-20 18:53:57 +0100

    Update comments for clarity and consistency

--- predictions_model.py
+++ predictions_model.py
@@ -1,16 +1,12 @@
 import pandas as pd
 import joblib
 
-# ===============================
-# 1. CARGAR EL MODELO
-# ===============================
+# 1. Cargamos el modelo
 model = joblib.load('models/modelo_precio_bmw.pkl')
 
 print("Introduce los datos del vehículo:\n")
 
-# ===============================
-# 2. PEDIR DATOS AL USUARIO
-# ===============================
+# 2. Pedimos los datos dek modelo al usuario
 model_car = input("Modelo (ej: 3 Series, X5, 1 Series): ")
 fuel_type = input("Tipo de combustible (Petrol / Diesel / Hybrid / Electric): ")
 transmission = input("Transmisión (Manual / Automatic / Semi-Auto): ")
@@ -21,9 +17,7 @@ year = int(input("Año del vehículo: "))
 # Conversión a km (IGUAL que en el entrenamiento)
 km = mileage_miles * 1.60934
 
-# ===============================
-# 3. CREAR DATAFRAME
-# ===============================
+# 3.Creamos el Dataframe 
 user_data = pd.DataFrame([{
     'model': model_car,
     'fuelType': fuel_type,
@@ -32,9 +26,8 @@ user_data = pd.DataFrame([{
     'year': year
 }])
 
-# ===============================
-# 4. PREDICCIÓN
-# ===============================
+
+# 4. Predicción
 prediction = model.predict(user_data)
 
 print("\n💰 Precio estimado del vehículo:")
